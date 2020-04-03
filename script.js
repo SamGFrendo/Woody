b6c493e9fd79d5bc8f1eee45533af705c548647e
Sam <sdgfrendo@hotmail.com>
2020-04-03 17:45:09 +0100

    commenting to find out how it works

--- script.js
+++ script.js
@@ -19,6 +19,7 @@ function init() {
     board.setAttribute("cellspacing", 0);
 
     // I'm not clear what this identifier is doing... 
+    // I think I really need to figure out what this identifer is doing... 
     var identifier = 1;
     //for statement depending on the size of the board 
     for (var i = 0; i < N_SIZE; i++) {
@@ -38,12 +39,16 @@ function init() {
                 if (j == N_SIZE - i - 1) {
                     cell.classList.add('diagonal1');
                 }
+            // It's wierd that I can't see an identifer in the DOM
             cell.identifier = identifier;
+            //This will call the set function on click
             cell.addEventListener("click", set);
             row.appendChild(cell);
             // We are pushing things on to the boxes array
             boxes.push(cell);
+            //identifier goes 1,2,4,8,16,32,64...
             identifier += identifier;
+            
             
         }
     }
@@ -66,6 +71,8 @@ function startNewGame() {
     };
     moves = 0;
     turn = "X";
+    //The forEach() method executes a provided function once for each array element.
+    //On init, set all boxes to empty
     boxes.forEach(function (square) {
         square.innerHTML = EMPTY;
     });
@@ -73,9 +80,11 @@ function startNewGame() {
 
 // Check if a win or not **************
 
+// I DO NOT UNDERSTAND HOW WIN IS DETERMINED 
 function win(clicked) {
     // Get all cell classes
     var memberOf = clicked.className.split(/\s+/);
+
     for (var i = 0; i < memberOf.length; i++) {
         var testClass = '.' + memberOf[i];
     var items = contains('#tictactoe ' + testClass, turn);
@@ -86,8 +95,10 @@ function win(clicked) {
     }
     return false;
 }
-
+// This function is called from the win fuction
 function contains(selector, text) {
+    //The Document method querySelectorAll() returns a static (not live) NodeList representing a list 
+    //of the document's elements that match the specified group of selectors.
     var elements = document.querySelectorAll(selector);
     return [].filter.call(elements, function(element){
         return RegExp(text).test(element.textContent);
@@ -97,19 +108,30 @@ function contains(selector, text) {
 // Sets clicked square and also updates the turn.**************
 
 function set() {
+    //If the cell is already filled in, don't do anything
+    //Need to find out more about 'this'
     if (this.innerHTML !== EMPTY) {
         return;
     }
+    //Set the cell to whoever's turn it is.
     this.innerHTML = turn;
     moves += 1;
+    //This just adds to the score object, depending on whose turn it is 
     score[turn] += this.identifier;
+    //console.log(score[turn]);
+    //console.log(score);
+
+    // Win function returns true or false
     if (win(this)) {
         alert('Winner: Player ' + turn);
         startNewGame();
+    // If no more moves can be made
     } else if (moves === N_SIZE * N_SIZE) {
         alert("Draw");
         startNewGame();
+    // Need to find out what '?' this does... 
     } else {
+        //turn altinator
         turn = turn === "X" ? "O" : "X";
         document.getElementById('turn').textContent = 'Player ' + turn;
     }
