dc13553a53862e6015fa60e5420865c8bd23fc8d
Sam <sdgfrendo@hotmail.com>
2020-04-18 19:36:20 +0100

    Includes comments that outline logic

--- readme.md
+++ readme.md
@@ -1,18 +1,45 @@
+# Woody
+
 https://guides.github.com/features/mastering-markdown/
 
-Got a TicTacToe game - https://codepen.io/vasanthkay/pen/KVzYzG 
+Origin of the TicTacToe game - https://codepen.io/vasanthkay/pen/KVzYzG. Can I take this, unpick the logic, and use it as the start of a coding project to determine 'a perfect game' in Woody. 
 
-Can I convert it to help me with woody
+Logic behind the tictactoe game... 
 
-I should think about how to create the different shaped block objects 
+All code is contained in functions. There are 5 functions;
+- init()
+- startNewGame()
+- win()
+- contains()
+- set()
+
+The function init() get called when the code is run. init() is also calls startNewGame(). Applying the different sets of classes to the cells is the way the code understands the realtionship between cells, and therefore how it determines the win criteria. 
+
+The function set() is called whenever a cell is clicked. The set() functions calls the win() function on each click to determine if the win criteria have been met. The set() function also sets a cell as being 'X' or 'O'.  
+
+The funcions win() and contains() work together to determine if win criteria have been met. 
+
+The way that the code determines if the win criteria has been met is complicated but quite elegant.  
+
+![console log output](./images/console-log-testClass-and-elements.png)
+
+When we click a cell we look at the set of all cells that could be completed with your turn counter (e.g. 'X' or 'O') to result in a win. We then count how many of this set contain the turn counter. If this = 3 then this turn wins. 
+
+**************************************************************************************************************
+
 
 First things to do...
-1. make the grid 10x10
-2. make it so there is only x
-3. make it so x actually blocks out the square
+1. Make it so there is only X
+2. Remove the concept of diagonals 
+3. make the grid 5x5 - This will be simple to do - just update the variable. This will allows us to start testing woody object...
+    The problem is I can't get these to be the correct height when I do this! 
 4. make it so a horrizontal or vertical row dissapears 
 
+Things to do...
+1. Find out more detail about the 'this' keyword in functions
 
+
+I should think about how to create the different shaped block objects 
 Woody objects 
 
 blocks
--- script.js
+++ script.js
@@ -2,26 +2,24 @@
 "use strict";
 
 var N_SIZE = 3,
-//Interestingly seems to work with a blank string as well as a non-breaking space
 EMPTY = "&nbsp;",
 boxes = [],
 turn = "X",
 score,
 moves;
 
-/*
-* Initializes the Tic Tac Toe board and starts the game.
-*/
+
+// Initialises the board in the UI and calls StartNewGame.
+
 function init() {
     // board is a table element
     var board = document.createElement('table');
     board.setAttribute("border", 1);
     board.setAttribute("cellspacing", 0);
 
-    // I'm not clear what this identifier is doing... 
-    // I think I really need to figure out what this identifer is doing... 
+    // 'identifier' is involved in adding to the score variable... not sure why
     var identifier = 1;
-    //for statement depending on the size of the board 
+    // Loops through and creates a table of size N_SIZE x N_SIZE
     for (var i = 0; i < N_SIZE; i++) {
         var row = document.createElement('tr');
         board.appendChild(row);
@@ -31,7 +29,8 @@ function init() {
             cell.setAttribute('width', 120);
             cell.setAttribute('align', 'center');
             cell.setAttribute('valign', 'center');
-            // add classes to the cell to determine if it is on the diagonal
+            // add classes to the cell to determine it's column position,
+            // row position and if it is one or more of two diagonals
             cell.classList.add('col' + j,'row' + i);
                 if (i == j) {
                     cell.classList.add('diagonal0');
@@ -39,32 +38,33 @@ function init() {
                 if (j == N_SIZE - i - 1) {
                     cell.classList.add('diagonal1');
                 }
-            // It's wierd that I can't see an identifer in the DOM
+            // Idintifier becomes a (property?) of the cell -
+            // depending on which cell is clicked this is added to score object
+
             cell.identifier = identifier;
             //This will call the set function on click
             cell.addEventListener("click", set);
+            //This is the equivalent of board.appendChild(row)
             row.appendChild(cell);
-            // We are pushing things on to the boxes array
+            // We are pushing 'cell' on to the boxes array
             boxes.push(cell);
             //identifier goes 1,2,4,8,16,32,64...
             identifier += identifier;
-            
-            
+
+
         }
     }
-    //boxes is an array of cells
-    console.log(boxes);
-
     //Here we append the board to an existing html element
     document.getElementById("tictactoe").appendChild(board);
     startNewGame();
 }
 
 
-// New game **************
+// New game ************** Sets the 'start' state of the initiliased board
 
 function startNewGame() {
-    //score is an object where 'X' and 'O' are intialised as 0
+    // score is an object where 'X' and 'O' are intialised as 0
+    // We don't need 'score' this can be removed. 
     score = {
         "X": 0,
         "O": 0
@@ -72,7 +72,6 @@ function startNewGame() {
     moves = 0;
     turn = "X";
     //The forEach() method executes a provided function once for each array element.
-    //On init, set all boxes to empty
     boxes.forEach(function (square) {
         square.innerHTML = EMPTY;
     });
@@ -80,14 +79,20 @@ function startNewGame() {
 
 // Check if a win or not **************
 
-// I DO NOT UNDERSTAND HOW WIN IS DETERMINED 
-function win(clicked) {
-    // Get all cell classes
-    var memberOf = clicked.className.split(/\s+/);
+// I DO NOT UNDERSTAND HOW WIN IS DETERMINED
+// When we define the function, the parameter is named 'clicked', when we call the function, we pass 'this' to it. 
+// 'this' = the clicked cell
 
+function win(clicked) {
+    //memberOf is an array containing the position description of the clicked cell  ["col2", "row0", "diagonal1"]
+    var memberOf = clicked.className.split(/\s+/);
     for (var i = 0; i < memberOf.length; i++) {
+        //test class creates a set of strings based on the array in memberOf
         var testClass = '.' + memberOf[i];
-    var items = contains('#tictactoe ' + testClass, turn);
+        //console.log('test class is ', testClass)
+        
+        var items = contains('#tictactoe ' + testClass, turn);
+        console.log(items) 
         // winning condition: turn == N_SIZE
         if (items.length == N_SIZE) {
             return true;
@@ -95,33 +100,41 @@ function win(clicked) {
     }
     return false;
 }
-// This function is called from the win fuction
+// This function is called from the win fuction - remember it is called multiple times for each memberOf the clicked cell
 function contains(selector, text) {
-    //The Document method querySelectorAll() returns a static (not live) NodeList representing a list 
+    //The Document method querySelectorAll() returns a NodeList representing a list
     //of the document's elements that match the specified group of selectors.
+    //'elements' is where the magic is - it is returning the set of cells that COULD lead to that player winning
+
     var elements = document.querySelectorAll(selector);
-    return [].filter.call(elements, function(element){
-        return RegExp(text).test(element.textContent);
-    });
+
+    /*
+    Uses a blank array in order to call the "filter" method. tells the filter to use 'elements' as the array to filter,
+    and only returns those cells which have the textContent = turn (textContent is 'set' each time a cell is clicked)
+
+    returning an array of those elements that contain the 'text' parameter, the text parameter is 'turn'  
+    */ 
+    // textContent is a property of the node!!! It is set as part of the set() function
+    return [].filter.call(elements, function(element){return RegExp(text).test(element.textContent);});
+
+ 
 }
 
 // Sets clicked square and also updates the turn.**************
-
+// 'onclick' this function is run -'this' is the cell that is clicked
 function set() {
-    //If the cell is already filled in, don't do anything
-    //Need to find out more about 'this'
-    if (this.innerHTML !== EMPTY) {
+    if (this.innerHTML !== EMPTY) { //If the cell is already filled in, don't do anything
         return;
     }
     //Set the cell to whoever's turn it is.
     this.innerHTML = turn;
     moves += 1;
-    //This just adds to the score object, depending on whose turn it is 
-    score[turn] += this.identifier;
+    //This just adds to the score object, depending on whose turn it is
+    score[turn] += this.identifier; //score is not needed and can be removed here
     //console.log(score[turn]);
     //console.log(score);
 
-    // Win function returns true or false
+    // Win function returns true or false -
     if (win(this)) {
         alert('Winner: Player ' + turn);
         startNewGame();
@@ -129,13 +142,13 @@ function set() {
     } else if (moves === N_SIZE * N_SIZE) {
         alert("Draw");
         startNewGame();
-    // Need to find out what '?' this does... 
+    // Need to find out what '?' this does...
     } else {
-        //turn altinator
+        //turn altinator - if it's X change to to O and visca versa
         turn = turn === "X" ? "O" : "X";
+        // This is simply updating the info text at the bottom of the page
         document.getElementById('turn').textContent = 'Player ' + turn;
     }
 }
 
-// Call init function to initialise the page
 init();
